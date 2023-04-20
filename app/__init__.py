9e2ef23268e52b77c564afcdab1ee40065b5e9f5
Tyler <edwardtyler.wilson@dcmail.ca>
2023-04-20 15:09:59 -0400

    modified:   app/__init__.py 	modified:   app/__pycache__/__init__.cpython-311.pyc 	modified:   templates/index.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
+import re
+
+from flask import Flask, render_template, request, flash, url_for, session, redirect
 
 
 
 
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder="/templates")
 
 
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('templates\index.html')
 
 
 
